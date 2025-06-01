fbeea4f2947921d8a603f00ced2da269e87a7bb2
Ted Stein <me@tedstein.net>
2025-05-31 18:46:26 -0700

    Refactor and marginally improve issue contents.

--- issues.py
+++ issues.py
@@ -1,18 +1,13 @@
+"""Utilities for interacting with and editing issues.
+
+Note that the bare word "repo" means a package repo in most of this codebase, but in
+this file, it means a code repo."""
+
+import datetime
 import json
 from typing import Optional
 
 from sh import gh
-
-
-def list_open_issues(repo: str) -> dict[str, str]:
-  """Return a dict of open issue titles to open issue urls in the given repo. Raises on
-  any error."""
-  # gh really, really wants to print rich text. _tty_out=False prevents this.
-  issues_json = gh(
-    "issue", "list", "-L", "9999", "-R", repo, "--json", "title,url", _tty_out=False
-  )
-  issues = json.loads(issues_json)
-  return {i["title"]: i["url"] for i in issues}
 
 
 def search_issues(repo: str, title: str) -> Optional[str]:
@@ -43,3 +38,19 @@ def open_new_issue(repo: str, title: str, body: str) -> str:
   Returns the URL of the new issue."""
   output = gh("issue", "create", "-R", repo, "-t", title, "-b", body)
   return output.splitlines()[-1]
+
+
+def issue_title(package_repo_domain: str, repo_name: str) -> str:
+  """Returns an issue title for the given package repo domain and name. This should be
+  stable, and is used for determining whether there's already an issue for a problem
+  with a given repo host."""
+  return f"[mirror-minder] {package_repo_domain} / {repo_name} is unhealthy"
+
+
+def issue_body(package_repo_domain: str, repo_name: str, reason: str) -> str:
+  """Returns an issue body appropriate for notifying humans of a problem."""
+  return f"""
+[`mirror-minder`](https://github.com/tstein/mirror-minder) has detected an issue with the `{repo_name}` repo on `https://{package_repo_domain}`: {reason}.
+
+Last updated: {datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d %H:%M:%S %Z")}
+    """.strip()
